810cf0c4690c69d5dde7e0ea9adc93e9d5ef9a17
Steven Kha <stevenvkha@gmail.com>
2018-04-03 10:57:49 -0700

    100%

--- cli.py
+++ cli.py
@@ -18,7 +18,7 @@ def recvAll(sock, numBytes):
     # declare the receive buffer
     recvBuff = ''
 
-    #loop until all files are received
+    # loop until all files are received
     while len(recvBuff) < numBytes:
 
         # initialize and declare the temporary buffer which can receives bytes
@@ -49,27 +49,27 @@ def createphmSocket(sportNum):
 
 # Function to upload a file to the server over an ephemeral port #
 def uploadFileToServer(fileName, sportNum):
-    # Generate an ephemeral port
+    # this is where you generate the ephemeral port
     print("    ", end=' ')
     tempSocket = createphmSocket(sportNum)  # function call from create socket
 
-    # Open file
+    # Open the file
     try:
-        file_object = open(fileName, 'r')
+        fio_object = open(fileName, 'r')
     except OSError:
         print("    ", fileName + 'cannot open.')
         tempSocket.close()
         return False
 
-    print("    ", 'Uploading ' + fileName + ' to the server')
+    print("    ", 'Now uploading ' + fileName + ' to the server')
     while True:
-        # Read data
-        data = file_object.read()
+        # Read  the data
+        data = fio_object.read()
 
-        # Make sure file is not empty by reading only EOF
+        # if file is not empty by reading only EOF
         if data:
 
-            # Get the size of the data read and convert it to string
+            # obtain the size and convert it to string data type
             dataSize = str(len(data))
 
             # Prepend 0's to the size string until the size is 10 bytes
@@ -88,46 +88,45 @@ def uploadFileToServer(fileName, sportNum):
             while len(data) > byteSent:
                 byteSent += tempSocket.send(data[byteSent:].encode(codingMethod))
 
-        # The file is completely empty
+        # The file is completely empty now
         else:
             break
 
         print("    ", 'Sent', byteSent, 'bytes.')
 
     # Close the socket and the file
-    file_object.close()
+    fio_object.close()
     tempSocket.close()
 
     return True
 
 
-# Function to download a file from the server over an ephemeral port #
+# Function to download a file from the server over the ephemeral port #
 def downloadFileFromServer(fileName, sportNum):
-    # Generate an ephemeral port
+    # ephmeral port is generated here
     print("    ", end=' ')
     tempSocket = createphmSocket(sportNum)
 
-    # Receive the first 10 bytes indicating the
-    # size of the file
+    # After receiving the first 10 bytes that indicates the size of the file
     bufferSize = recvAll(tempSocket, 10)
 
-    # Get the file size
+    # file size obtained
     if bufferSize == '':
         print("    ", 'Missing data.')
         return False
     else:
         fileSize = int(bufferSize)
 
-    print("    ", 'The file is', fileSize, 'bytes long')
+    print("    ", 'The size of the file is', fileSize, 'bytes long')
 
-    # Get the file data
+    # obtaining the file data
     # demux the data from the server to the client until receive buffer is full
     data = recvAll(tempSocket, fileSize)
 
-    # Open file to write to
+    # Open file and then write to file
     fWriter = open(fileName, 'w+')
 
-    # Write received data to file
+    # Write data to the file
     fWriter.write(data)
 
     # Close file
@@ -136,100 +135,109 @@ def downloadFileFromServer(fileName, sportNum):
     return True
 
 
-# *******************************************************************
-#                    MAIN PROGRAM
-# *******************************************************************
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                    MAIN PROGRAM STARTS HERE
+# *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def main():
     if len(sys.argv) < 2:
+        # if command line has less than 2 arguments,
         print('python' + sys.argv[0] + '<server_port>')
+        # illustrate how to enter the server port to connect to server
+
     serverName = "ecs.fullerton.edu"
     serverPort = int(sys.argv[1])
+    # user inputs the server port for the print message above
 
+    # create an socket for the server port and label as the primary socket
     pSocket = createphmSocket(serverPort)
 
+    # loop until user enters quit
     while True:
         ans = input('ftp> ')
+        # Write a command after ftp> print
 
-        # Argument counting using spaces
+        # Argument counting is space sensitive
         ftp_arg_count = ans.count(' ')
 
         if ftp_arg_count == 1:
             (command, fileName) = ans.split()
+            # split the commands if more than one word to command and then filename
         elif ftp_arg_count == 0:
             command = ans
+            # one string long command is
 
-        # Process input
+        # Input command
         if command == 'put' and ftp_arg_count == 1:
-            # Send the entire command to server: put [file]
+            # The put [file]command is sent to server
             pSocket.send(ans.encode(codingMethod))
 
-            # Receive an ephemeral port from server to upload the file over
+            # client receives the ephemeral port address from the server to upload the file over
             tempPort = pSocket.recv(bufferSize).decode(codingMethod)
 
-            print("    ", 'Received the ephemeral port #', tempPort)
-            success = uploadFileToServer(fileName, tempPort)
+            print("    ", 'Gotcha! Obtained the ephemeral port #', tempPort)
+            uploaded = uploadFileToServer(fileName, tempPort)
 
-            if success:
-                print("    ", 'Successfully uploaded the file')
-                # Get server report
+            if uploaded:
+                print("    ", 'Completed uploading the file')
+                # Get the server status
                 receipt = pSocket.recv(1).decode(codingMethod)
                 if receipt == '1':
-                    print("    ", 'Server successfully has received file')
+                    print("    ", 'Congrats, the *Server* has received the file')
                 else:
-                    print("    ", 'Server was not able to receive the file')
+                    print("    ", 'oops! Server did not receive the file')
             else:
-                print("    ", 'Unable to upload the file')
+                print("    ", 'Not able to upload the file')
 
         elif command == 'get' and ftp_arg_count == 1:
-            # Send the entire command to server: get [file]
+            # The get [file]command is sent to server
             pSocket.send(ans.encode(codingMethod))
 
-            # Receive an ephemeral port from server to download the file over
+            # client receives the ephemeral port address from the server to upload the file over
             tempPort = pSocket.recv(bufferSize).decode(codingMethod)
-            print("    ", 'Received the ephemeral port #', tempPort)
+            print("    ", 'Gotcha! Obtained the ephemeral port #', tempPort)
 
-            success = downloadFileFromServer(fileName, tempPort)
+            uploaded = downloadFileFromServer(fileName, tempPort)
 
-            # Send success/failure notification to server
-            if success:
-                print("    ", 'Server has successfully downloaded the file')
+            # Send success/failure status to the server
+            if uploaded:
+                print("    ", 'Congrats, the *Server* has downloaded the file!')
                 pSocket.send('1'.encode(codingMethod))
             else:
-                print("    ", 'Unable to download the file')
+                print("    ", 'dummit! Unable to download the file')
                 pSocket.send('0'.encode(codingMethod))
 
         elif command == 'dir' and ftp_arg_count == 0:
-            # Send the entire command to server: ls
+            # if file not able to download the file send the command to server: ls
             pSocket.send(ans.encode(codingMethod))
 
-            # Get ephemeral port generated by server
+            # obtain the ephemeral port generated by the server
             tempPort = pSocket.recv(bufferSize).decode(codingMethod)
-            print("    ", 'Received the ephemeral port #', tempPort)
+            print("    ", 'Yay! Received the ephemeral port #', tempPort)
 
-            # Create ephemeral socket and wait for data
+            # ephemeral socket and is waiting for the data
             print("    ", end=' ')
             ephmSocket = createphmSocket(tempPort)
             data = ephmSocket.recv(bufferSize)
 
-            # Need 'pickle.loads' to extract list
+            # Extract list
             server_dir = pickle.loads(data)
 
-            # Print out directory
-            print('\n', "    " + 'Files that are on the server:')
+            # Print directory
+            print('\n', "    " + 'number of files that are on the server:')
             for line in server_dir:
                 print("    ", line)
 
             ephmSocket.close()
 
         elif command == 'quit' and ftp_arg_count == 0:
-            print("    ", 'Closing it now')
+            print("    ", 'ending the session now')
 
             pSocket.send(ans.encode(codingMethod))
             pSocket.close()
             break
 
         else:
-            print("    ", 'Invalid command. Try: putting [file], getting [file], ls, or quit')
+            print("    ", 'not a valid command. Try: put [file], get [file], ls, or quit')
 
 
 if __name__ == "__main__":
